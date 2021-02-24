7edb502897fdc8f9e697c3ed33d30bb2ac8909ae
Ola Ishola <olaishola@hotmail.co.uk>
2021-02-24 23:45:54 +0100

    views

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const blogs = require("./assets/blogs");
+const morgan = require("morgan");
 
 const app = express();
 
 app.set("view engine", "ejs");
+
+// middleware
+app.use(morgan("dev"));
+// app.use((req, res, next) => {
+//     console.log("new request made");
+//     console.log("host: ", req.hostname);
+//     console.log("path: ", req.path);
+//     console.log("method: ", req.method);
+
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log("in the next middleware");
+
+//     next();
+// });
+
+// middleware for static files
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Home", blogs });
